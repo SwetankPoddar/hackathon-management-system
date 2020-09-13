7591c85903496145004c0f6bedd2f90a22db6baf
Frazz3l <46572332+Frazz3l@users.noreply.github.com>
2020-09-13 23:39:37 +0100

    [added] category functionality
    -category model
    -category view
    -add category view
    -add cat form
    -added cat urls
    -updated population script
    -added cat to admin.py

--- hackathonSystem/admin.py
+++ hackathonSystem/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Challenge, RequestsMade, Team, CustomUser
+from .models import Challenge, RequestsMade, Team, CustomUser, Category
 
 # Register your models here.
 admin.site.register(Challenge)
 admin.site.register(RequestsMade)
 admin.site.register(Team)
 admin.site.register(CustomUser)
+admin.site.register(Category)
--- populate_db.py
+++ populate_db.py
@@ -4,7 +4,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE','GUTS.settings')
 import django
 django.setup()
 
-from hackathonSystem.models import Challenge,CustomUser,RequestsMade,Team,User
+from hackathonSystem.models import Challenge,CustomUser,RequestsMade,Team,User,Category
 
 import random
 
@@ -12,7 +12,7 @@ def print_bar():
     print('---------------')
 
 def create_team(user_acc, team_name, member_details, hackerrank_accounts ):
-    print('Creating a team account with the name' + team_name)
+    print('Creating a team account with the name ' + team_name)
     return Team.objects.create(
             user = user_acc,
             name = team_name,
@@ -32,39 +32,55 @@ def create_user(user_name, password, type_of_user):
         user_object.is_staff = True
 
     user_object.save()
-    
+
     customUser = CustomUser.objects.create(
         user=user_object,
         user_type=type_of_user
     )
     print_bar()
     return customUser
-    
-    
-def create_challenge(name, points, description):
+
+
+def create_challenge(name, points, description, category):
     print('Creating challenge with the following infomration:')
     print('name: ' + name)
     print('points: ' + str(points))
     print('description: ' + description)
-    
+    print('category: ' + str(category))
+
     print_bar()
 
     return Challenge.objects.create(
         name = name,
         points_avaliable = points,
-        description = description
+        description = description,
+        category = category,
     )
-    
+
+
+
+def create_category(name):
+    print('Creating category with the following infomration:')
+    print('name: ' + name)
+
+    print_bar()
+
+    return Category.objects.create(
+        name = name
+    )
+
+
 def create_request(team, challenge):
     pass
 
 
 if __name__ == "__main__":
-    
+
     admin_user = create_user('admin_acc2', 'password', 2)
+    cat = create_category("GUTS")
 
     for i in range(5):
-        create_challenge(f'challenge-{i+1}', random.randint(1,11)*10, f'description-{i+1}')
+        create_challenge(f'challenge-{i+1}', random.randint(1,11)*10, f'description-{i+1}',cat)
 
     for i in range(5):
         team_user = create_user(f'team_acc{i+1}', 'password', 1)
@@ -74,4 +90,3 @@ if __name__ == "__main__":
             f'memberdetails-{i+1}',
             f'hackerrank-{i+1}'
         )
-
